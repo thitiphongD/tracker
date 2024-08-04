c7283f2c21bf4643312fc8a1aff39a5a02f9ef96
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-08-04 20:49:27 +0700

    build navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { ImBug } from "react-icons/im";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
+      <Link href={"/"}>
+        <ImBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
