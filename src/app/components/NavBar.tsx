0b475ceadbafb82561b8ab6dc0250f6b233be82c
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-08-04 21:02:02 +0700

    style active link

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { ImBug } from "react-icons/im";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +23,14 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            // className={`${
+            //   link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+            // }  hover:text-zinc-800 transition-colors`}
             href={link.href}
           >
             {link.label}
