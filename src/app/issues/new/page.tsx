e61fa24c3c529bfca991f6418b2eb315440a219d
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-08-05 13:37:59 +0700

    build error message component

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import SimpleMDEWrapper from "@/app/components/SimpleMDEWrapper";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -47,11 +48,7 @@ const NewIssuePage = () => {
           placeholder="Title"
           {...register("title")}
         ></TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -59,11 +56,7 @@ const NewIssuePage = () => {
             <SimpleMDEWrapper placeholder="Description" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button>Submit new issue</Button>
       </form>
