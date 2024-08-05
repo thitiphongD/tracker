79b2dd9b866e4d800df2ec5c222a53c4146d7232
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-08-05 15:53:27 +0700

    add and config NextAuth

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -3,16 +3,16 @@ import { createIssueSchema } from "@/app/validationSchema";
 import { PrismaClient } from "@prisma/client";
 
 export async function POST(request: NextRequest) {
-    const prisma = new PrismaClient();
-    const body = await request.json();
-    const validation = createIssueSchema.safeParse(body);
-    if (!validation.success) {
-        return NextResponse.json(validation.error.format(), { status: 400 })
-    }
+  const prisma = new PrismaClient();
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
 
-    const newIssue = await prisma.issue.create({
-        data: { title: body.title, description: body.description }
-    })
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
 
-    return NextResponse.json(newIssue, { status: 201 })
+  return NextResponse.json(newIssue, { status: 201 });
 }
--- src/app/validationSchema.ts
+++ src/app/validationSchema.ts
@@ -1,22 +1,30 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const createIssueSchema = z.object({
-    title: z.string().min(1, 'Title is require').max(255),
-    description: z.string().min(1, 'Description is require')
-})
+  title: z.string().min(1, "Title is require").max(255),
+  description: z.string().min(1, "Description is require"),
+});
 
-export const createUserSchema = z.object({
+export const createUserSchema = z
+  .object({
     name: z.string().trim(),
     email: z.string().email("Invalid email format"),
-    password: z.string()
-        .min(8, { message: "Password must be at least 8 characters long" })
-        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
-        .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
-        .regex(/[0-9]/, { message: "Password must contain at least one number" })
-        .regex(/[@$!%*?&]/, { message: "Password must contain at least one special character" }),
-    confirmPassword: z.string()
-})
-    .refine((data) => data.password === data.confirmPassword, {
-        message: "Passwords do not match",
-        path: ["confirmPassword"],
-    });
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .regex(/[0-9]/, { message: "Password must contain at least one number" })
+      .regex(/[@$!%*?&]/, {
+        message: "Password must contain at least one special character",
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
