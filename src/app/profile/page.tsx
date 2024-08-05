b633acb0c33289c591f00e87d26a473add7841ea
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-08-05 17:26:30 +0700

    catch if not have user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,15 +1,32 @@
 "use client";
 
-import React from "react";
-import { useSession } from "next-auth/react";
+import React, { useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
+import { Button } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
   const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status !== "authenticated") {
+      router.push("/");
+    }
+  }, [router, session, status]);
+
   return (
     <>
       {status === "authenticated" && session?.user && (
         <div>
           <p>hello {session?.user.email}</p>
+          <Button
+            onClick={() => signOut({ callbackUrl: "/" })}
+            color="red"
+            variant="soft"
+          >
+            Sign out
+          </Button>
         </div>
       )}
     </>
